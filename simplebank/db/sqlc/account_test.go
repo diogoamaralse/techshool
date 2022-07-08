8f90f296f9ff491c40e87e2f3a40ae93322b10f7
Diogo Amaral <diogocoelhoamaral@gmail.com>
2022-07-08 15:46:00 +0100

    Account Unit Test Done

--- simplebank/db/sqlc/account_test.go
+++ simplebank/db/sqlc/account_test.go
@@ -1,7 +1,26 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestCreateAccount(t *testing.T) {
-	
+	arg := CreateAccountParams{
+		Owner:    "Tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
 }
