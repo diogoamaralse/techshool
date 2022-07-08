74bd7dde255b53d5b94244a6f435a0d3dd0bde26
Diogo Amaral <diogocoelhoamaral@gmail.com>
2022-07-08 16:09:16 +0100

    random unit values

--- simplebank/db/sqlc/account_test.go
+++ simplebank/db/sqlc/account_test.go
@@ -3,14 +3,15 @@ package db
 import (
 	"context"
 	"github.com/stretchr/testify/require"
+	"techschool/simplebank/util"
 	"testing"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "Tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
