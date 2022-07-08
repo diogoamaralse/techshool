3f980f91baea179d99d948f963974e26d383ca02
Diogo Amaral <diogocoelhoamaral@gmail.com>
2022-07-08 10:46:58 +0100

    SQLC AUTO GENERATE

---
+++ simplebank/db/query/account.sql
@@ -0,0 +1,15 @@
+-- name: CreateAccount :one
+insert into accounts
+    (owner, balance, currency)
+values ($1,$2,$3)
+    returning *;
+
+-- name: GetAccount :one
+select * from accounts
+where id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * from accounts
+order by id
+    LIMIT $1
+offset $2;
